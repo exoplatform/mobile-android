32a2aea92b158041d999703ec8b7f698f5ead4b9
stevan_lemeur <stevan_lemeur>
2011-06-08 15:35:59 +0000

    MOB-330 : Add activities mocks for Android

---
+++ src/eXo/eXoPlatform/mocks/SocialActivity.java
@@ -0,0 +1,26 @@
+package eXo.eXoPlatform.mocks;
+
+import java.util.Date;
+
+public class SocialActivity {
+	
+	public String userID;
+	public String title;
+	public String body;
+    public Date lastUpdateDate;
+    public long postedTime;
+    public int nbLikes;
+    public int nbComments;
+    
+    
+	public SocialActivity(String userID, String title, String body,
+			long postedTime, int nbLikes, int nbComments) {
+		super();
+		this.userID = userID;
+		this.title = title;
+		this.body = body;
+		this.postedTime = postedTime;
+		this.nbLikes = nbLikes;
+		this.nbComments = nbComments;
+	}
+}
