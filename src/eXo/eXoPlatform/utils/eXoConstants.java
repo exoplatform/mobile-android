7be2920ce3ae0e764ff39fc73803fd0ea044c498
Duong Dai Nguyen <duongnd@exoplatform.com>
2011-07-11 02:41:37 +0000

    MOB-326: repair utils package

---
+++ src/eXo/eXoPlatform/utils/eXoConstants.java
@@ -0,0 +1,29 @@
+package eXo.eXoPlatform.utils;
+
+public class eXoConstants {
+
+  public static final String COMPOSE_TYPE              = "COMPOSE_TYPE";
+
+  public static final String ACTIVITY_DETAIL_EXTRA     = "ACTIVITY_DETAIL_EXTRA";
+
+  public static final int    COMPOSE_POST_TYPE         = 0;
+
+  public static final int    COMPOSE_COMMENT_TYPE      = 1;
+
+  public static final int    TAKE_PICTURE_WITH_CAMERA  = 7;
+
+  public static final String ACTIVITY_PROTOCOL         = "http";
+
+  public static final String ACTIVITY_HOST             = "192.168.1.12";
+
+  public static final int    ACTIVITY_PORT             = 8080;
+
+  public static final String ACTIVITY_PORTAL_CONTAINER = "socialdemo";
+
+  public static final String ACTIVITY_REST_CONTEXT     = "rest-socialdemo";
+
+  public static final String ACTIVITY_REST_VERSION     = "v1-alpha1";
+
+  public static final String ACTIVITY_ORGANIZATION     = "organization";
+
+}
