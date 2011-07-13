97ff0ba7e2eac4b213ad5722dfdc834cf983ed4b
Duong Dai Nguyen <duongnd@exoplatform.com>
2011-07-13 06:04:31 +0000

    MOB-377:refactor package

---
+++ src/exo/exoplatform/chat/ExoChatMessageContent.java
@@ -0,0 +1,17 @@
+package exo.exoplatform.chat;
+
+//Message info
+public class ExoChatMessageContent {
+
+  public String name;   // Message owner
+
+  public String content; // Content
+
+  // Constructor
+  public ExoChatMessageContent(String strName, String strContent) {
+    name = strName;
+    content = strContent;
+
+  }
+
+}
