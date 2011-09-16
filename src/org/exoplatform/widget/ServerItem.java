13ef7d6f47c6fbb9486bbc50309985b920d00b9f
Duong Dai Nguyen <duongnd@exoplatform.com>
2011-09-16 03:12:07 +0000

    MOB-562: Create Setting Controller

---
+++ src/org/exoplatform/widget/ServerItem.java
@@ -0,0 +1,29 @@
+package org.exoplatform.widget;
+
+import org.exoplatform.R;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class ServerItem extends RelativeLayout {
+
+  public TextView  serverName;
+
+  public TextView  serverUrl;
+
+  public ImageView serverImageView;
+
+  public ServerItem(Context context) {
+    super(context);
+    LayoutInflater inflate = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    View view = inflate.inflate(R.layout.serverlistitemforsetting, this);
+    serverName = (TextView) view.findViewById(R.id.TextView_ServerName);
+    serverUrl = (TextView) view.findViewById(R.id.TextView_URL);
+    serverImageView = (ImageView) view.findViewById(R.id.ImageView_Checked);
+  }
+
+}
