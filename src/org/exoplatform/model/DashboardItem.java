5dca29dd255e27f45c6e1f032f518ea6043712f6
Xuyen Mai <xuyen.mai@exoplatform.com>
2011-10-27 11:18:20 +0000

    MOB-507: add DashboardItem class

---
+++ src/org/exoplatform/model/DashboardItem.java
@@ -0,0 +1,24 @@
+package org.exoplatform.model;
+
+//	Dashboard tab
+public class DashboardItem {
+
+  public String          id;   // Dashboard ID
+
+  public String          html;    // Dashboard html
+
+  public String          link;    // Dashboard link
+
+  public String          label;    // Dashboard link
+
+  // Constructor
+
+  public DashboardItem(String _id, String _html, String _link, String _label) {
+
+    id = _id;
+    html = _html;
+    link = _link;
+    label = _label;
+
+  }
+}
