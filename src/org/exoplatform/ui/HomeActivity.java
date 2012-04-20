fa38b44577c97b02981f360fd549f21ff152cb78
Nguyen Dai Duong <duongnd@exoplatform.com>
2012-04-20 14:52:28 +0700

    MOB-1102:[Android] Avoid memory leak when turn back to our application

--- src/org/exoplatform/ui/HomeActivity.java
+++ src/org/exoplatform/ui/HomeActivity.java
@@ -2,10 +2,16 @@ package org.exoplatform.ui;
 
 import greendroid.widget.ActionBarItem;
 
+import java.util.ArrayList;
+
 import org.exoplatform.R;
 import org.exoplatform.controller.home.HomeActionListenner;
 import org.exoplatform.controller.home.HomeAdapter;
 import org.exoplatform.controller.home.HomeController;
+import org.exoplatform.singleton.AccountSetting;
+import org.exoplatform.singleton.DocumentHelper;
+import org.exoplatform.singleton.ServerSettingHelper;
+import org.exoplatform.utils.ExoConnectionUtils;
 import org.exoplatform.utils.ExoConstants;
 import org.exoplatform.widget.MyActionBar;
 
@@ -17,6 +23,14 @@ import android.view.Window;
 import android.widget.GridView;
 
 public class HomeActivity extends MyActionBar {
+  private static final String SERVER_SETTING_HELPER = "SERVER_SETTING_HELPER";
+
+  private static final String ACCOUNT_SETTING       = "account_setting";
+
+  private static final String DOCUMENT_HELPER       = "document_helper";
+
+  private static final String COOKIESTORE           = "cookie_store";
+
   private GridView            gridView;
 
   private HomeController      homeController;
@@ -24,14 +38,35 @@ public class HomeActivity extends MyActionBar {
   private HomeActionListenner homeActionListenner;
 
   @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+  public void onCreate(Bundle bundle) {
+    super.onCreate(bundle);
     requestWindowFeature(Window.FEATURE_NO_TITLE);
     setActionBarContentView(R.layout.home_layout);
     super.getActionBar().setType(greendroid.widget.ActionBar.Type.Dashboard);
     addActionBarItem();
     getActionBar().getItem(0).setDrawable(R.drawable.action_bar_logout_button);
+    if (bundle != null) {
+      DocumentHelper helper = bundle.getParcelable(DOCUMENT_HELPER);
+      DocumentHelper.getInstance().setInstance(helper);
+      AccountSetting accountSetting = bundle.getParcelable(ACCOUNT_SETTING);
+      AccountSetting.getInstance().setInstance(accountSetting);
+      ServerSettingHelper settingHelper = bundle.getParcelable(SERVER_SETTING_HELPER);
+      ServerSettingHelper.getInstance().setInstance(settingHelper);
+      ArrayList<String> cookieList = bundle.getStringArrayList(COOKIESTORE);
+      ExoConnectionUtils.setCookieStore(ExoConnectionUtils.cookiesStore, cookieList);
+    }
     init();
+  }
+
+  @Override
+  protected void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+    outState.putParcelable(SERVER_SETTING_HELPER, ServerSettingHelper.getInstance());
+    outState.putParcelable(ACCOUNT_SETTING, AccountSetting.getInstance());
+    outState.putParcelable(DOCUMENT_HELPER, DocumentHelper.getInstance());
+    outState.putStringArrayList(COOKIESTORE,
+                                ExoConnectionUtils.getCookieList(ExoConnectionUtils.cookiesStore));
+
   }
 
   @Override
