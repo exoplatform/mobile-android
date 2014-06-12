51d14fee3f08ec9339a91bc758456e6f16cf8f8c
Arnaud Héritier <aheritier@exoplatform.com>
2014-06-12 13:16:52 +0200

    Cleanup surefire config : * Use a test suite to enforce the order of execution. Otherwise the upgrade of Junit, Surefire, Maven, Java changes the order and tests are failing * Don't enforce the surefire version * Define argLine as a project property instead of a plugin configuration to be compatible with jacoco

---
+++ test/org/exoplatform/mobile/tests/TestSuite.java
@@ -0,0 +1,14 @@
+package org.exoplatform.mobile.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * @author Arnaud Héritier <aheritier@exoplatform.com>
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({WelcomeActivityTest.class, LoginActivityTest.class, SignInOnPremiseActivityTest.class,
+                        LaunchActivityTest.class, ExoEmailsAndURLsValidationTest.class, SettingsActivityTest.class,
+                        HomeActivityTest.class})
+public class TestSuite {
+}
